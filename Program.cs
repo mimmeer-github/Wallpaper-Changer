fad7efa6615d3ec016607746b5952530c83497b2
Mimmeer <archietablet99@gmail.com>
2024-12-28 10:33:17 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string userDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}";
+if (args.Length == 0)
+{
+    string filePath = userDir + "\\wall\\0.jpg";
+    string extra = "\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper";
+    string fullPath = userDir + extra;
+    Console.WriteLine(fullPath + "  " + filePath);
+    File.Delete(fullPath);
+    File.Copy(filePath, fullPath);
+}
+else 
+{
+    string filePath = args[0];
+    string extra = "\\AppData\\Roaming\\Microsoft\\Windows\\Themes\\TranscodedWallpaper";
+    string fullPath = userDir + extra;
+    Console.WriteLine(fullPath + "  " + filePath);
+    File.Delete(fullPath);
+    File.Copy(filePath, fullPath);
+}
